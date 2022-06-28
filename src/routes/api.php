38f020a7993abce56c7ed52fea609a5cec839cc1
Reinaldo <reinaldo.marquez@nazaries.com>
2022-06-28 20:50:22 -0300

    first commit

---
+++ src/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Attributes:
+
+// films -- The URL root for Film resources
+// people -- The URL root for People resources
+// planets -- The URL root for Planet resources
+// species -- The URL root for Species resources
+// starships -- The URL root for Starships resources
+// vehicles -- The URL root for Vehicles resources
+
+// Route::get('/swapi/{attribute}', function ($attribute) {
+//     $response = Http::get('http://swapi.dev/api/' . $attribute);
+//     return $response;
+// });
